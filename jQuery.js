1731e229d66331d3ab96e71920fca97eeb4a5f51
Alexander <berezkin05@gmail.com>
2018-03-17 16:40:27 +0200

    have no idea what I did...

--- jQuery.js
+++ jQuery.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 
+    if ($(window).width() > 480) {
     $(".hide-big").hover(function(){
         $(".dropdown-content").show(100);
     });
+    } else {
+        $(".hide-big").on("touchstart", function(){
+            $(".dropdown-content").show(100);
+        });
+    }
 
     $(this).on("click", function(){
         $(".dropdown-content").hide();
