fe5c42b29e4ff9acaeb14c2243dd67d5d3453067
Alexander <berezkin05@gmail.com>
2018-03-17 16:43:47 +0200

    have no idea what I did...

--- jQuery.js
+++ jQuery.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
     if ($(window).width() > 480) {
-    $(".hide-big").hover(function(){
+    $(".icon").hover(function(){
         $(".dropdown-content").show(100);
     });
     } else {
-        $(".hide-big").on("touchstart", function(){
+        $(".icon").on("touchstart", function(){
             $(".dropdown-content").show(100);
         });
     }
