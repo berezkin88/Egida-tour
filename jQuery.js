ccc7dad08dd1e4a080fefef68b507f53aea90e89
Alexander <berezkin05@gmail.com>
2018-03-17 17:39:55 +0200

    something stats to work) add id to div

--- jQuery.js
+++ jQuery.js
@@ -1,16 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    if ($(window).width() > 480) {
-    $(".icon").hover(function(){
-        $(".dropdown-content").show(100);
+    $("#hover").hover(function(){
+        $(".dropdown-content").show(300);
     });
-    } else {
-        $(".icon").on("touchstart", function(){
-            $(".dropdown-content").toggle(500);
-        });
-    }
 
-    $(this).on("click", function(){
+
+    $(this).on("click", function () {
         $(".dropdown-content").hide();
     });
 });
